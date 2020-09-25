b79b6d0d5e671462a5355d71e98af57db314b6f8
aya240 <aya03308@gmail.com>
2020-09-25 05:29:43 -0700

    aa

---
+++ js/j.js
@@ -0,0 +1,57 @@
+var can = document.getElementById('c'),
+mycontext = can.getContext('2d');
+
+mycontext.fillStyle='rgba(100,20,10,0.43)';
+mycontext.fillRect(0, 0, 1280, 400);
+
+mycontext.font='140px Arial';
+mycontext.lineWidth =4;
+mycontext.strokeStyle='#ddd';
+
+mycontext.fillStyle='#00f';
+mycontext.strokeText('E', 200, 180);
+mycontext.fillText('E', 200, 180);
+
+
+
+mycontext.fillStyle='#901';
+mycontext.strokeText('d', 300, 150);
+mycontext.fillText('d', 300, 150);
+
+
+mycontext.fillStyle='#012';
+mycontext.strokeText('u',380, 190);
+mycontext.fillText('u', 380, 190);
+
+
+
+mycontext.fillStyle='#120';
+mycontext.strokeText('G',470, 140);
+mycontext.fillText('G', 470, 140);
+
+
+
+mycontext.fillStyle='#009';
+mycontext.strokeText('a',580,200);
+mycontext.fillText('a', 580, 200);
+
+
+
+mycontext.fillStyle='#f00';
+mycontext.strokeText('m',660, 180);
+mycontext.fillText('m', 660 ,180);
+
+
+
+mycontext.fillStyle='#410';
+mycontext.strokeText('e',770, 240);
+mycontext.fillText('e', 770, 240);
+
+
+
+
+
+mycontext.font='70px Arial';
+
+mycontext.fillStyle='#fff';
+mycontext.fillText('welcome', 400, 340);
